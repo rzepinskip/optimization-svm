7162840b695c38b7f14532ec2ee0b2d4e43b6b8e
Zuzanna Magierska <zuzia.magierska@gmail.com>
2019-12-17 16:01:04 +0100

    Load data from datasets

--- opt.py
+++ opt.py
@@ -1,11 +1,22 @@
 import cvxpy as cp
 import numpy as np
+import pandas as pd
 
-n = 10
 C = 15
 
-z = 2 * np.random.randint(2, size=n) - 1
-y = np.random.randn(n, n)
+# data = pd.read_csv('datasets/breastCancer.csv')
+# n = data.shape[0]
+# y = data.loc[:, (data.columns != 'diagnosis') & (data.columns != 'id')].to_numpy()
+# z = data.loc[:, 'diagnosis'].replace('B', -1).replace('M', 1).to_numpy()
+
+data = pd.read_csv('datasets/diabetes.csv')
+n = data.shape[0]
+y = data.loc[:, data.columns != 'Outcome'].to_numpy()
+z = data.loc[:, 'Outcome'].replace(0, -1).to_numpy()
+
+# z = 2 * np.random.randint(2, size=n) - 1
+# y = np.random.rand(n, n) * 100000
+
 Q = np.zeros((n, n))
 for i in range(n):
     for j in range(n):
@@ -20,7 +31,7 @@ prob = cp.Problem(
 )
 prob.solve()
 
-# Print result.
+# Print result. 
 print("\nThe optimal value is", prob.value)
 print("A solution x is")
 print(alpha.value)
