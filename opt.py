3ca9c2cea42ee0330e60b0f093f80fd0b3754e66
Zuzanna Magierska <zuzia.magierska@gmail.com>
2019-12-17 22:39:39 +0100

    Split to train and test sets

--- opt.py
+++ opt.py
@@ -3,26 +3,29 @@ import numpy as np
 import pandas as pd
 
 
-def read_data(file, className):
+C = 15
+className = 'Outcome'
+
+
+def read_data(file):
     data = pd.read_csv(file)
     for column in data:
         if column != className:
             data[column] = (data[column] - data[column].min()) / (data[column].max() - data[column].min())
 
-    return data, data.shape[0]
+    return data
+
+def get_vars_and_classes(data):
+    y = data.loc[:, data.columns != className].to_numpy()
+    z = data.loc[:, className].replace(0, -1).to_numpy()
+    return y, z
 
 
-C = 15
-
-# className = 'diagnosis'
-# data, n = read_data('datasets/breastCancer.csv', className)
-# y = data.loc[:, (data.columns != className) & (data.columns != 'id')].to_numpy()
-# z = data.loc[:, className].replace('B', -1).replace('M', 1).to_numpy()
-
-className = 'Outcome'
-data, n = read_data('datasets/diabetes.csv', className)
-y = data.loc[:, data.columns != className].to_numpy()
-z = data.loc[:, className].replace(0, -1).to_numpy()
+data = read_data('datasets/diabetes.csv')
+msk = np.random.rand(len(data)) < 0.8
+y, z = get_vars_and_classes(data[msk])
+y_test, z_test = get_vars_and_classes(data[~msk])
+n = y.shape[0]
 
 Q = np.zeros((n, n))
 for i in range(n):
@@ -37,9 +40,16 @@ prob = cp.Problem(
 )
 prob.solve()
 
-# Print result. 
 print("\nThe optimal value is", prob.value)
 print("A solution x is")
 print(alpha.value)
 print("A dual solution is")
 print(prob.constraints[0].dual_value)
+
+# we should add calculated h() function for accordingly train and test data 
+# using alpha.value or prob.constraints[0].dual_value or something else (?)
+
+# train_error = (np.sign(alpha.value @ y) != np.sign(z @ y)).sum() / n
+# print(train_error)
+# test_error = (np.sign(alpha.value @ y_test) != np.sign(z_test @ y_test)).sum() / y_test.shape[0]
+# print(test_error)
